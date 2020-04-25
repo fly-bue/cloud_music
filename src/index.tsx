03054d550566b00c505039e8a9d7e3d35c18cd9d
fly-bue <phcFly@163.com>
2020-04-25 09:19:13 +0800

    less and antd-m import

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Button } from 'antd-mobile';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Button type="primary">Start</Button>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
